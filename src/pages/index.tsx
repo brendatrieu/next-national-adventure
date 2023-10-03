831bd592c34d30921d4e201c8509a59da81a3688
Brenda Trieu <trieubrenda@gmail.com>
2023-10-03 13:47:19 -0700

    Adjusted padding for page headers.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import ParkCard from '@/components/layout/ParkCard/ParkCard';
 export default function Home() {
   return (
     <section>
-      <h2>National Parks</h2>
+      <h2 style={{ paddingTop: '1rem' }}>National Parks</h2>
       <ParkCard />
     </section>
   );
